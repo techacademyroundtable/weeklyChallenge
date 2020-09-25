9c11880c08dd9362f05d0dbcd0c7473bd623cc1f
Forest Moher <fomoher@gmail.com>
2020-09-24 18:29:09 -0700

    September 23/ 26 coding challenges

---
+++ Sept-26  Challenge 2/Sept-26  Challenge 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept_26__Challenge_2
+{
+    class Program
+    {
+
+        static string DoubleChar(string str)
+        {
+            var finalStr = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+                finalStr += str[i].ToString() + str[i].ToString();
+            }
+            return finalStr;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            var stri = Console.ReadLine();
+            Console.WriteLine(DoubleChar(stri));
+            Console.Read();
+        }  
+    }
+}
