e876fe27fa43f05fea3525407d07992f6e00b28f
Forest Moher <fomoher@gmail.com>
2020-10-15 16:39:37 -0700

    October 14th challenges

---
+++ Oct-14 CSharp Challenge/Oct-14 CSharp Challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oct_14_CSharp_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {4, 5, 2, 6, 1};
+            missingNum(arr);
+            Console.ReadLine();
+        }
+
+        static void missingNum(int[] num)
+        {
+            Array.Sort(num);
+            foreach (int i in num) {
+                if (!num.Contains(i + 1) && i != num.Length + 1){
+                    Console.WriteLine(i + 1);
+                };
+            }
+            
+            
+        }
+    }
+}
